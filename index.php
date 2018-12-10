2f5b4a9a837855fd4b777a273ecce851ce90c9ec
Kevin Sedano Carrillo <43473373+ksedano@users.noreply.github.com>
2018-12-10 16:52:57 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>TASKLIST</title>
 </head>
 <body>
-	HOLA
+	<h1>Floating beyond... TASKLIST</h1>
+	<form>
+		Nova tasca:
+		<input name="nom" />
+		<input type="submit" />
+	</form>
+	<p>Tasques pendents:</p><ul>
+	<li>Nois, a programar!!!! <a href='?fet=62'>Fet</a> <a href='?esborra=62'>Esborra</a></li>
+	<li>quedar per fer unes birres <a href='?fet=63'>Fet</a> <a href='?esborra=63'>Esborra</a></li>
+	<li>fasf <a href='?fet=59'>Fet</a> <a href='?esborra=59'>Esborra</a></li>
+	</ul>
+	Tasques fetes:</p>
+	<ul>
+	<li>eadsd <a href='?desfes=58'>Desfes</a> <a href='?esborra=58'>Esborra</a></li>
+	<li>hola <a href='?desfes=57'>Desfes</a> <a href='?esborra=57'>Esborra</a></li>
 </body>
 </html>
